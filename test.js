62064b29f5f8862c79539c2aad48cfd7ca02d8be
Lance Gundersen <LanceGundersen@users.noreply.github.com>
2019-01-20 13:08:11 -0500

    Added for loop.

--- test.js
+++ test.js
@@ -1,3 +1,10 @@
 // Initial test commit by Kyle
 
 console.log("Hello World!");
+
+const array = [0,1,2,3];
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    console.log("Element: " + element);
+}
