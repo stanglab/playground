6ee50a05e908c6d2878e484b15e17b336a0bbcda
flansah <46830141+flansah@users.noreply.github.com>
2019-01-26 23:23:43 -0500

    Update and rename README.md to Kasi.js
    Pull request practice

---
+++ Kasi.js
@@ -0,0 +1,11 @@
+{
+  "name": "git-test again",
+  "version": "1.1.0",
+  "description": " Just exercuting pull request",
+  "main": "GitTest.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "author": "",
+  "license": "JKL"
+}
